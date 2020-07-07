8ac74c238c7831209991211c75a48f31ff581fbe
G-SM <42806190+G-SM@users.noreply.github.com>
2020-07-08 01:12:17 +0530

    Settings and wsgi

--- Socify/settings.py
+++ Socify/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'socify.urls'
+ROOT_URLCONF = 'Socify.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'socify.wsgi.application'
+WSGI_APPLICATION = 'Socify.wsgi.application'
 
 
 # Database
--- Socify/wsgi.py
+++ Socify/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simplesocial.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Socify.settings")
 
 application = get_wsgi_application()
